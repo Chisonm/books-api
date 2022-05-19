406e88a3ce6612b71cfb453583c4cfe7f3c6015c
chisonm <danielchisonum@gmail.com>
2022-05-19 12:35:35 +0100

    test to check if comment body has more than 500 character

--- tests/Unit/CommentsTest.php
+++ tests/Unit/CommentsTest.php
@@ -55,4 +55,22 @@ class CommentsTest extends TestCase
             'Success' => false,
         ]);
     }
+
+    // test_check_if_body_is_500_characters
+
+    public function test_check_if_body_is_500_characters(){
+        $book = Book::factory()->create();
+        $params = [
+            'book_id'   => $book->id,
+            'name'   => 'daniel',
+            'body'   => 'Lorem ipsum dolor daneil sit amet, consectetuer adipiscing elit. Aenean commodo ligula eget dolor. Aenean massa. Cum sociis natoque penatibus et magnis dis parturient montes, nascetur ridiculus mus. Donec quam felis, ultricies nec, pellentesque eu, pretium quis, sem. Nulla consequat massa quis enim. Donec pede justo, fringilla vel, aliquet nec, vulputate eget, arcu. In enim justo, rhoncus ut, imperdiet a, venenatis vitae, justo. Nullam dictum felis eu pede mollis pretium. Integer tincidunt. Cras dapibu',
+            'ip'   => '127.0.0.1',
+        ];
+        $response = $this->json('POST', '/api/comments', $params, ['Accept' => 'application/json']);
+        $response
+         ->assertStatus(422, $response->status())
+         ->assertJson([
+            'Success' => false,
+        ]);
+    }
 }
