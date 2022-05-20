edb89167be443531a1d987968602b59b42d889c0
chisonm <danielchisonum@gmail.com>
2022-05-20 14:45:11 +0100

    refactored controller

--- app/Http/Controllers/Api/CommentsController.php
+++ app/Http/Controllers/Api/CommentsController.php
@@ -3,8 +3,12 @@
 namespace App\Http\Controllers\Api;
 
 use Illuminate\Http\Request;
+use App\utils\ApiCustomResponse;
 use App\Services\CommentsService;
+use Illuminate\Support\Facades\DB;
 use App\Http\Controllers\Controller;
+use App\Http\Resources\CommentsResource;
+use Symfony\Component\HttpFoundation\Response;
 
 class CommentsController extends Controller
 {
@@ -21,7 +25,14 @@ class CommentsController extends Controller
      */
     public function index()
     {
-        return $this->commentService->getAll();
+        try {
+            $comments = $this->commentService->getAll();
+            $message = "Comments retrieved successfully!";
+            return ApiCustomResponse::successResponse($message, CommentsResource::collection($comments), Response::HTTP_OK);
+        } catch (\Exception $e) {
+            $message = 'Something went wrong while processing your request.';
+            return ApiCustomResponse::errorResponse($message, Response::HTTP_INTERNAL_SERVER_ERROR, $e);
+        }
     }
 
     /**
@@ -32,7 +43,20 @@ class CommentsController extends Controller
      */
     public function store(Request $request)
     {
-        return $this->commentService->createComment($request->all());
+        DB::beginTransaction();
+        try {
+            $comment = $this->commentService->createComment($request->all());
+            $message = "Comment created successfully!";
+            DB::commit();
+            return ApiCustomResponse::successResponse($message, new CommentsResource($comment), Response::HTTP_CREATED);
+        } catch (InvalidArgumentException $e) {
+            DB::rollback();
+            $message = $e->getMessage();
+            return ApiCustomResponse::errorResponse($message, Response::HTTP_UNPROCESSABLE_ENTITY, $e);
+        } catch (\Exception $e) {
+            DB::rollback();
+            $message = 'Something went wrong while processing your request.';
+            return ApiCustomResponse::errorResponse($message, Response::HTTP_INTERNAL_SERVER_ERROR, $e);
+        }
     }
-
 }
--- app/Services/CommentsService.php
+++ app/Services/CommentsService.php
@@ -21,48 +21,29 @@ class CommentsService
     // get all comments
     public function getAll()
     {
-        try {
-            $comments = $this->commentsRepository->getAll();
+        $comments = $this->commentsRepository->getAll();
 
-            if (!$comments) {
-                $message = "No comments found!";
-                return ApiCustomResponse::errorResponse($message, Response::HTTP_NO_CONTENT);
-            }
-            $message = "Comments retrieved successfully!";
-            return ApiCustomResponse::successResponse($message, CommentsResource::collection($comments), Response::HTTP_OK);
-        } catch (\Exception $e) {
-            $message = 'Something went wrong while processing your request.';
-            return ApiCustomResponse::errorResponse($message, Response::HTTP_INTERNAL_SERVER_ERROR, $e);
+        if (!$comments) {
+            $message = "No comments found!";
+            return ApiCustomResponse::errorResponse($message, Response::HTTP_NO_CONTENT);
         }
+        return $comments;
     }
 
     // create comment
     public function createComment(array $data)
     {
-        DB::beginTransaction();
-        try {
-            $validator = Validator::make($data, [
+        $validator = Validator::make($data, [
             'book_id' => 'required|integer|exists:books,id',
             'name' => 'required|string|max:255',
             'body' => 'required|string|max:500',
         ]);
-            if ($validator->fails()) {
-                throw new InvalidArgumentException($validator->errors()->first());
-            }
-
-            $data['ip'] = request()->ip();
-            $comment = $this->commentsRepository->create($data);
-            $message = "Comment created successfully!";
-            DB::commit();
-            return ApiCustomResponse::successResponse($message, new CommentsResource($comment), Response::HTTP_CREATED);
-        } catch (InvalidArgumentException $e) {
-            DB::rollback();
-            $message = $e->getMessage();
-            return ApiCustomResponse::errorResponse($message, Response::HTTP_UNPROCESSABLE_ENTITY, $e);
-        } catch (\Exception $e) {
-            DB::rollback();
-            $message = 'Something went wrong while processing your request.';
-            return ApiCustomResponse::errorResponse($message, Response::HTTP_INTERNAL_SERVER_ERROR, $e);
+        if ($validator->fails()) {
+            throw new InvalidArgumentException($validator->errors()->first());
         }
+
+        $data['ip'] = request()->ip();
+        $comment = $this->commentsRepository->create($data);
+        return $comment;
     }
 }
